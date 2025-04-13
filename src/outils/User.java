bca5e75a2b8ec5d13cc3fab7356aed5a97e63f72
RohyfuturBillionaire <rohyrajoelina@gmail.com>
2025-04-13 19:04:10 +0300

    auth controller and user util

---
+++ src/outils/User.java
@@ -0,0 +1,22 @@
+package outils;
+
+public class User {
+    String username;
+    String role;
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+}
